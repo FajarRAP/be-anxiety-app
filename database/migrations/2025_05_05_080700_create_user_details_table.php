17a45c63251c4b51e4ac0bf1837e08cd932ded11
Fajar Riansyah <fajary781@gmail.com>
2025-05-05 15:20:02 +0700

    feat: add UserDetail model and migration for user details table

---
+++ database/migrations/2025_05_05_080700_create_user_details_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->integer('age')->nullable();
+            $table->float('height')->nullable();
+            $table->float('weight')->nullable();
+            $table->enum('genders', ['MALE', 'FEMALE'])->nullable();
+            $table->enum('education', ['SD', 'SMP', 'SMA', 'D1', 'D2', 'D3', 'D4', 'S1', 'S2', 'S3', 'NONE'])->nullable();
+            $table->string('occupation')->nullable();
+            $table->enum('marriage', ['KAWIN', 'BELUM KAWIN', 'CERAI HIDUP', 'CERAI MATI'])->nullable();
+            $table->string('duration')->nullable();
+            $table->string('history')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
