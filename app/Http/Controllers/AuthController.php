d8fb0fdd75af6486a230f0ab9943a0341c848e6c
Fajar Riansyah <fajary781@gmail.com>
2025-05-05 16:28:59 +0700

    feat: implement AuthController with registration, login, and get user use case

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile;
+use App\Models\User;
+use App\Models\UserDetail;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Console\Scheduling\Event;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => ['required', 'string', 'max:255', 'unique:' . User::class],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(
+                ['errors' => $validator->errors()],
+                422
+            );
+        }
+
+        $user = User::create([
+            'username' => $request->username,
+            'password' => bcrypt($request->password),
+        ]);
+        Profile::create([
+            'user_id' => $user->id,
+        ]);
+
+        event(new Registered($user));
+
+        return response()->json([
+            'message' => 'User registered successfully',
+            'data' => [
+                'user' => $user,
+            ],
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => ['required', 'string'],
+            'password' => ['required', 'string'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $user = User::where('username', $request->username)->first();
+        $checkPassword = Hash::check($request->password, $user->password);
+
+        if (!$user || !$checkPassword) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'User logged in successfully',
+            'data' => [
+                'token' => $user->createToken($user->username)->plainTextToken,
+                'user' => $user->toResource(),
+            ],
+        ]);
+    }
+
+    public function user(Request $request)
+    {
+        $user = $request->user();
+
+        return response()->json([
+            'message' => 'User retrieved successfully',
+            'data' => [
+                'user' => $user->toResource(),
+            ],
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/', fn() => 'HALO BRO');
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/auth/register', 'register');
+    Route::post('/auth/login', 'login');
+    Route::get('/auth/user', 'user')->middleware('auth:sanctum');
+});
