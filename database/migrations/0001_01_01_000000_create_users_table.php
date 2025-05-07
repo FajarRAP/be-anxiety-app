3582c46dcfc665111566fe7f7202bb43196d4557
Fajar Riansyah <fajary781@gmail.com>
2025-05-07 14:48:05 +0700

    refactor (user migration): improve foreign key constraint definition for sessions table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,10 @@ return new class extends Migration
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->foreignUuid('user_id')->nullable()->index();
+            $table->foreignUuid('user_id')
+                ->constrained()
+                ->nullable()
+                ->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
