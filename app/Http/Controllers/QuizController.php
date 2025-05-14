af0d11e6c4fcc1b105594d87a1e9999a9698beaf
Fajar Riansyah <fajary781@gmail.com>
2025-05-15 00:18:05 +0700

    refactor (quiz controller): restructure index method to group quizzes by type and format response

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -3,14 +3,58 @@
 namespace App\Http\Controllers;
 
 use App\Models\Quiz;
+use App\Models\QuizType;
 
 class QuizController extends Controller
 {
     public function index()
     {
+        $quizTypes = QuizType::all();
+        $quizzes = Quiz::all()->toResourceCollection()->collection->groupBy('quiz_type_id');
+
+        $mapQuizzes = fn($quizzes) => $quizzes->map(fn($quiz) =>
+        [
+            'id' => $quiz->id,
+            'question' => $quiz->question,
+        ])->values();
+
         return response()->json([
             'message' => 'Success retrieve quizzes',
-            'data' => Quiz::all()->toResourceCollection(),
+            'data' => [
+                [
+                    'quiz_type' => [
+                        'id' => $quizTypes[0]->id,
+                        'name' => 'HARS',
+                    ],
+                    'quizzes' => $quizzes->get($quizTypes[0]->id)->groupBy('title')->map(fn($group, $title) => [
+                        'title' => $title,
+                        'questions' => $mapQuizzes($group),
+                    ])->values(),
+                ],
+                [
+                    'quiz_type' => [
+                        'id' => $quizTypes[1]->id,
+                        'name' => 'MSPSS',
+                    ],
+                    'quizzes' => $mapQuizzes($quizzes->get($quizTypes[1]->id)),
+                ],
+                [
+                    'quiz_type' => [
+                        'id' => $quizTypes[2]->id,
+                        'name' => 'DQoL',
+                    ],
+                    'quizzes' => [
+                        [
+                            'type' => 'often',
+                            'questions' => $mapQuizzes($quizzes->get($quizTypes[2]->id)->splice(7)),
+                        ],
+                        [
+                            'type' => 'satisfcation',
+                            'questions' => $mapQuizzes($quizzes->get($quizTypes[2]->id)),
+                        ],
+                    ],
+                ],
+            ],
         ]);
     }
 }
